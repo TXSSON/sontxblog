53585ec21ba99d08659caff63bfe3f3047514cb7
SON <txuanson95@gmai.com>
2025-09-09 22:21:09 +0700

    create project

---
+++ src/main/java/com/sontx/blog/SontxblogApplication.java
@@ -0,0 +1,13 @@
+package com.sontx.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SontxblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SontxblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sontx/blog/SontxblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sontx.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SontxblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
